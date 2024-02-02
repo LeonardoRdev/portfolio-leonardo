be53c6d600d5c33d7713966990e5bd3112485772
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2024-02-02 16:03:16 -0300

    Função para fazer com que mostre os icones clicaveis dos projetos

--- script.js
+++ script.js
@@ -2,16 +2,26 @@ const projeto = document.querySelectorAll(".projeto");
 
 projeto.forEach(function(itemProjeto) {
     itemProjeto.addEventListener("mouseover", function() {
-        const texto = this.querySelector("p");
-        // this.style.backgroundColor = "red"
+        const texto = this.querySelector("p.descricao-projeto");
+
         this.style.marginBottom = "100px";
-        texto.style.opacity = "100%"
+        texto.style.opacity = "100%";
+
+        const iconesProjeto = this.querySelectorAll(".icones_para_acessar_projeto");
+        iconesProjeto.forEach((itemIcone) => {
+            itemIcone.style.opacity = "100%"
+        })
     })
 
     itemProjeto.addEventListener("mouseout", function() {
-        const texto = this.querySelector("p")
-        // alert("saiu")
-        texto.style.opacity = "0"
-        this.style.marginBottom = "0"
+        const texto = this.querySelector("p.descricao-projeto");
+
+        texto.style.opacity = "0";
+        this.style.marginBottom = "0";
+
+        const iconesProjeto = this.querySelectorAll(".icones_para_acessar_projeto");
+        iconesProjeto.forEach((itemIcone) => {
+            itemIcone.style.opacity = "0";
+        })
     })
 })
