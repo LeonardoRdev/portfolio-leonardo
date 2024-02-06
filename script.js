c7108afec0ca350dfd1b81e7814b48657d3e61d8
Leonardo Rocha <leonardorochaconstantino@gmail.com>
2024-02-06 18:17:18 -0300

    Ajustes no código

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// Projetos -> animação de aparecer o texto e os ícones
+
 const projeto = document.querySelectorAll(".projeto");
 
-projeto.forEach(function(itemProjeto) {
-    itemProjeto.addEventListener("mouseover", function() {
+projeto.forEach(function (itemProjeto) {
+    itemProjeto.addEventListener("mouseover", function () {
         const texto = this.querySelector("p.descricao-projeto");
 
         this.style.marginBottom = "100px";
@@ -13,7 +15,7 @@ projeto.forEach(function(itemProjeto) {
         })
     })
 
-    itemProjeto.addEventListener("mouseout", function() {
+    itemProjeto.addEventListener("mouseout", function () {
         const texto = this.querySelector("p.descricao-projeto");
 
         texto.style.opacity = "0";
@@ -24,4 +26,14 @@ projeto.forEach(function(itemProjeto) {
             itemIcone.style.opacity = "0";
         })
     })
+})
+
+
+
+// Contato -> enviar mensagem
+
+const botaoEnviar = document.querySelector("#botao-enviar");
+
+botaoEnviar.addEventListener("click", () => {
+    alert("Mensagem enviada!")
 })
